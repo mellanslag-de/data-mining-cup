3ad350f29127091d277d9c313c3bad7b7539f2f7
Rootkey <p.rutkiewitz@web.de>
2019-04-08 11:56:23 +0200

    Code entschlackt
    Code an Vorlage aus https://www.researchgate.net/publication/324438530_Hyperparameters_and_Tuning_Strategies_for_Random_Forest angepasst

--- tuneRanger_RF.R
+++ tuneRanger_RF.R
@@ -1,3 +1,7 @@
+install.packages("devtools")
+devtools::install_github("PhilippPro/tuneRanger")
+library(tuneRanger)
+library(mlr)
 library(data.table)
 
 train <- read.table("../Pfad/train.csv",header=T,sep=",")
@@ -7,35 +11,25 @@ submission <- read.table("../Pfad/submission.csv",header=T,sep=",")
 
 
 set.seed(42)
-install.packages("devtools")
-install.packages("rlang")
-install.packages("mlr")
-library(devtools)
-library(rlang)
-devtools::install_github("PhilippPro/tuneRanger")
-library(tuneRanger)
-library(mlr)
+
 # A mlr task has to be created in order to use the package
-#data(train)
+
 train<-train[0:150,]
 
 #ID Code entfernen weil unnötig
 train$ID_code<-NULL
-#train$target<-as.factor(train$target)
-#na.omit(train)
-train.task = makeRegrTask(data = train, target ="target")
-?makeClassifTask
+
+train.task = makeClassifTask(data = train, target ="target")
+makeClassifTask
 
 estimateTimeTuneRanger(train.task)
 
-res =tuneRanger(train.task, 
-                 
+res =tuneRanger(train.task,
+                measure =list(multiclass.brier),
                 num.trees =3000,
                 num.threads =2,
                 iters =200,
                 iters.warmup =30)
-#measure =list(multi),
-?tuneRanger
 
 res
 
